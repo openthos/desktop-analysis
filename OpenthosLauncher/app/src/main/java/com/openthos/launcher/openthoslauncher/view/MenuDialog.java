11da9130d1cf7d687f482f2a2e66d065f9d60427
0123hhtx <0123hhtx@163.com>
2016-09-26 16:23:27 +0800

    Update MenuDialog.java
    给菜单增加单例

--- OpenthosLauncher/app/src/main/java/com/openthos/launcher/openthoslauncher/view/MenuDialog.java
+++ OpenthosLauncher/app/src/main/java/com/openthos/launcher/openthoslauncher/view/MenuDialog.java
@@ -37,6 +37,7 @@ public class MenuDialog extends Dialog {
     private Context context;
     private Type type;
     private String path;
+    private static MenuDialog menuDialog;
 
     public MenuDialog(Context context) {
         super(context);
@@ -58,6 +59,13 @@ public class MenuDialog extends Dialog {
     protected MenuDialog(Context context, boolean cancelable, DialogInterface.OnCancelListener cancelListener) {
         super(context, cancelable, cancelListener);
         this.context = context;
+    }
+    
+    public static MenuDialog getInstance(Context context, Type type, String path) {
+        if (menuDialog == null) {
+           menuDialog = new MenuDialog(context,type,path);
+        }
+        return menuDialog;
     }
 
     @Override
