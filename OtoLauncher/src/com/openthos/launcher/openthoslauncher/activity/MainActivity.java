1c3606e5811488ffb188dd3b3d98cc44cb0921d1
0123hhtx <0123hhtx@163.com>
2016-10-10 10:00:57 +0800

    Update MainActivity.java

--- OtoLauncher/src/com/openthos/launcher/openthoslauncher/activity/MainActivity.java
+++ OtoLauncher/src/com/openthos/launcher/openthoslauncher/activity/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends Launcher implements RecycleCallBack {
     private ItemTouchHelper mItemTouchHelper;
     public static Handler mHandler;
     private int mHeightNum = OtoConsts.MAX_LINE;
-    private boolean mIsExistMene = false;
     private boolean mIsClicked = false;
     private boolean mIsRename = false;
 
@@ -222,15 +221,16 @@ public class MainActivity extends Launcher implements RecycleCallBack {
 
             @Override
             public boolean onTouch(View view, MotionEvent event) {
-                if (event.getButtonState() == MotionEvent.BUTTON_SECONDARY) {
-                    if (!mIsExistMene) {
-                        MenuDialog dialog = MenuDialog.getInstance(MainActivity.this,
-                                                                   Type.blank, "/");
-                        dialog.showDialog((int) event.getRawX(), (int) event.getRawY());
-                    } else {
-                        mIsExistMene = false;
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (event.getButtonState() == MotionEvent.BUTTON_SECONDARY) {
+                        if (!MenuDialog.isExistMenu()) {
+                            MenuDialog dialog = MenuDialog.getInstance(MainActivity.this,
+                                                                       Type.blank, "/");
+                            dialog.showDialog((int) event.getRawX(), (int) event.getRawY());
+                        } else {
+                            MenuDialog.setExistMenu(false);
+                        }
                     }
-                }
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     if (!mIsClicked && mAdapter.pos != -1) {
                         mAdapter.getData().get(mAdapter.pos).put("isChecked", false);
@@ -251,16 +251,17 @@ public class MainActivity extends Launcher implements RecycleCallBack {
             
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getButtonState() == MotionEvent.BUTTON_SECONDARY) {
-                    if (!mIsExistMene) {
-                        MenuDialog dialog = MenuDialog.getInstance(MainActivity.this,
-                                                                   Type.blank, "/");
-                        dialog.showDialog((int) event.getRawX(), (int) event.getRawY());
-                    } else {
-                       mIsExistMene = false;
-                    }
-                }
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (event.getButtonState() == MotionEvent.BUTTON_SECONDARY) {
+                        if (!MenuDialog.isExistMenu()) {
+                            MenuDialog dialog = MenuDialog.getInstance(MainActivity.this,
+                                                                       Type.blank, "/");
+                            dialog.showDialog((int) event.getRawX(), (int) event.getRawY());
+                        } else {
+                           MenuDialog.setExistMenu(false);
+                        }
+                    }
+
                     if (!mIsClicked && mAdapter.pos != -1) {
                         mDatas.get(mAdapter.pos).put("isChecked", false);
                         mAdapter.pos = -1;
